e38f709be6b4cab208058145c83ca87d265ef235
mkoertgen <marcel.koertgen@gmail.com>
2015-12-31 10:25:59 +0100

    Enhanced class name

--- AutoFac.TestingHelpers/AssertionExtensions.cs
+++ AutoFac.TestingHelpers/AssertionExtensions.cs
@@ -1,31 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using Autofac;
-using Module = Autofac.Module;
+﻿using Autofac;
 
 namespace AutoFac.TestingHelpers
 {
     public static class AssertionExtensions
     {
-        public static IContainer Container<TModule>(this TModule module, 
-            Action<ContainerBuilder> arrange = null, IEnumerable<Type> types = null)
-            where TModule : Module, new()
-        {
-            var builder = Builder(module, arrange, types);
-            return builder.Build();
-        }
-
-        public static MockContainerBuilder Builder<TModule>(this TModule module, 
-            Action<ContainerBuilder> arrange = null, IEnumerable<Type> types = null)
-            where TModule : Module, new()
-        {
-            var builder = new MockContainerBuilder();
-            if (types != null) builder.Substitute(types);
-            arrange?.Invoke(builder);
-            builder.RegisterModule(module);
-            return builder;
-        }
-
         public static ContainerAssertions Should(this IContainer container)
         {
             return new ContainerAssertions(container);
