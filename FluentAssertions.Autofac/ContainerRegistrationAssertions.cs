387054aae899b2107d32907b0ff102865eb0a3cc
Körtgen <marcel.koertgen@gmail.com>
2016-02-19 10:33:29 +0100

    fixed naming & subclassing

--- FluentAssertions.Autofac/ContainerRegistrationAssertions.cs
+++ FluentAssertions.Autofac/ContainerRegistrationAssertions.cs
@@ -1,32 +1,35 @@
 ﻿using System;
+using System.Diagnostics;
 using Autofac;
+using FluentAssertions.Primitives;
 
 namespace FluentAssertions.Autofac
 {
-    public class ContainerRegistrationAssertions
+    [DebuggerNonUserCode]
+    public class ContainerRegistrationAssertions : ReferenceTypeAssertions<IContainer, ContainerRegistrationAssertions>
     {
-        private readonly IContainer _container;
+        protected override string Context => nameof(IContainer);
 
-        public ContainerRegistrationAssertions(IContainer container)
+        public ContainerRegistrationAssertions(IContainer subject)
         {
-            if (container == null) throw new ArgumentNullException(nameof(container));
-            _container = container;
+            if (subject == null) throw new ArgumentNullException(nameof(subject));
+            Subject = subject;
         }
 
         public RegisterAssertions Registered<TService>()
         {
-            return new RegisterAssertions(_container, typeof(TService));
+            return new RegisterAssertions(Subject, typeof(TService));
         }
 
         public RegisterAssertions Registered(Type type)
         {
-            return new RegisterAssertions(_container, type);
+            return new RegisterAssertions(Subject, type);
         }
 
         public RegisterAssertions Registered(object instance)
         {
             if (instance == null) throw new ArgumentNullException(nameof(instance));
-            return new RegisterAssertions(_container, instance.GetType());
+            return new RegisterAssertions(Subject, instance.GetType());
         }
 
         public void NotRegistered<TService>()
@@ -36,7 +39,7 @@ namespace FluentAssertions.Autofac
 
         public void NotRegistered(Type type)
         {
-            _container.IsRegistered(type).Should().BeFalse($"Type '{type}' should not be registered");
+            Subject.IsRegistered(type).Should().BeFalse($"Type '{type}' should not be registered");
         }
 
         public void NotRegistered<TService>(string serviceName)
@@ -46,7 +49,7 @@ namespace FluentAssertions.Autofac
 
         public void NotRegistered(string serviceName, Type type)
         {
-            _container.IsRegisteredWithName(serviceName, type).Should()
+            Subject.IsRegisteredWithName(serviceName, type).Should()
                 .BeFalse($"Type '{type}' should not be registered with name '{serviceName}'");
         }
 
@@ -57,7 +60,7 @@ namespace FluentAssertions.Autofac
 
         public void NotRegistered(object serviceKey, Type type)
         {
-            _container.IsRegisteredWithKey(serviceKey, type).Should()
+            Subject.IsRegisteredWithKey(serviceKey, type).Should()
                 .BeFalse($"Type '{type}' should not be registered with key '{serviceKey}'");
         }
     }
