4cd52ef6add69037535e01211b07011ed6c0ccde
Marcel Körtgen <marcel.koertgen@gmail.com>
2018-10-20 20:33:14 +0200

    Streamline nuget packaging

--- FluentAssertions.Autofac.NuGet/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("FluentAssertions.Autofac.NuGet")]
-[assembly: Guid("2afdbfa5-8e08-4839-a57f-4dccb3f80837")]
