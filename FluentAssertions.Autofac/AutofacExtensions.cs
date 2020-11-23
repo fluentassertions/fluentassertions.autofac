dd04354cf0003bbd44d25bfefd896a46589f8bd1
Marcel Körtgen <marcel.koertgen@gmail.com>
2020-11-23 07:49:17 +0100

    Add strong name

--- FluentAssertions.Autofac/AutofacExtensions.cs
+++ FluentAssertions.Autofac/AutofacExtensions.cs
@@ -4,7 +4,7 @@ using Autofac;
 using Autofac.Core;
 
 using System.Runtime.CompilerServices;
-[assembly: InternalsVisibleTo("FluentAssertions.Autofac.Tests")]
+[assembly: InternalsVisibleTo("FluentAssertions.Autofac.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010001d021d48ad15ae00f3d868ab095ef510f7757370a881a21578b0a36846b78af9d8c80968157f9a44d7b9861eb914b31cbd14d5e28317ab85f4aa32b21f7006cb8cae758954081835fc6c26ce2c4965dbb664e93e02b2f5b4bbface294ed968a22a8f02832cd065f1627af932fbb41229adc042ccdc9bed67487bbb7b17d2ff6")]
 
 namespace FluentAssertions.Autofac
 {
